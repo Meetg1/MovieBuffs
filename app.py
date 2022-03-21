16e9398e08f9a19eb2a9162fca8aa4b3f6f39c5f
shubhgosalia <shubh604@gmail.com>
2022-03-21 14:08:14 +0530

    added home page ui

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Home!</p>"
+def home():
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=7000)
